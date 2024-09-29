7b3a2ae65b2a1c03598510ef7e0e187b338aefcd
Aishwarya Shankar <1396aishu@gmail.com>
2024-09-29 23:03:49 +1000

    Update main and scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,3 +1,5 @@
+##
+
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -1,6 +1,10 @@
+
+## Push the images
 set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
  
 docker push $CONTAINER_REGISTRY.azurecr.io/book-catalog:latest
 docker push $CONTAINER_REGISTRY.azurecr.io/inventory-management:latest
 
+docker pull $CONTAINER_REGISTRY.azurecr.io/book-catalog:latest
+docker pull $CONTAINER_REGISTRY.azurecr.io/inventory-management:latest
