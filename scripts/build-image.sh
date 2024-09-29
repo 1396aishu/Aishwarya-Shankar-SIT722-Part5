8040f306d4a06de40a51abd04c231e60668d2e92
Aishwarya Shankar <1396aishu@gmail.com>
2024-09-29 20:40:55 +1000

    updated scripts

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -1,7 +1,10 @@
+set -u # or set -o nounset
+: "$CONTAINER_REGISTRY"
+
 # Build the Docker image
 docker-compose up --build -d
  
-# Tag the Docker image
+# Tag the Docker images
  
-docker tag book-catalog:$VERSION $CONTAINER_REGISTRY.azurecr.io/book-catalog:$VERSION
-docker tag inventory-management:$VERSION $CONTAINER_REGISTRY.azurecr.io/inventory-management:$VERSION
+docker tag book-catalog:latest $CONTAINER_REGISTRY.azurecr.io/book-catalog:latest
+docker tag inventory-management:latest $CONTAINER_REGISTRY.azurecr.io/inventory-management:latest
--- scripts/push-image.sh
+++ scripts/push-image.sh
@@ -2,5 +2,6 @@ set -u # or set -o nounset
 : "$CONTAINER_REGISTRY"
 : "$VERSION"
 
+
 docker push $CONTAINER_REGISTRY.azurecr.io/book-catalog:$VERSION
 docker push $CONTAINER_REGISTRY.azurecr.io/inventory-management:$VERSION
